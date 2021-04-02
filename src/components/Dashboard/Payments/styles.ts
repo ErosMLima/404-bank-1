bdb551cc66f9ec91eae52a9fd9cb612b8d2b75b3
Driele An <drielean@gmail.com>
2021-04-02 13:11:16 -0300

    responsividade

--- src/components/Dashboard/Payments/styles.ts
+++ src/components/Dashboard/Payments/styles.ts
@@ -3,25 +3,26 @@ import styled from "styled-components"
 export const CardContainer = styled.div`
   width: 100vw;
   max-height: 80vh;
-
   display: flex;
   justify-content: center;
   align-items: center;
-
   background: #E5E5E5;
   padding: 73px;
+
+  @media screen and (max-width: 900px){
+    width: 90%;
+    height: 100%;
+  }
 `
 
 export const CardDashboard = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
-
   padding: 42px;
   border-radius: 10px;
-
   background-color: #fdf9f9;
-  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+  box-shadow: #1d385699 1px 1px 4px 1px;
 
   .container {
     width: 100%;
