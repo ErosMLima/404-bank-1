870867e6aafe841483efe82c68393b9aa8b9b102
Tomas Meneses <tomasmeneses22@gmail.com>
2021-04-04 11:12:16 -0300

    remove last months filter, add check cpf and check full name toast and validation

--- src/Pages/Landing/index.tsx
+++ src/Pages/Landing/index.tsx
@@ -59,7 +59,6 @@ const Landing: React.FC = () => {
       password &&
       confirmPassword &&
       cpf.length === 11 &&
-      checkCpf(cpf) &&
       username &&
       passwordMatch) setIsFilled(true)
     else setIsFilled(false)
@@ -97,6 +96,16 @@ const Landing: React.FC = () => {
       })
 
       if (password !== confirmPassword) {
+        return
+      }
+
+      if(!checkCpf(cpf)) {
+        toast.error('CPF Inválido')
+        return
+      }
+
+      if(!checkNameAndLastName(name)) {
+        toast.error('Digite o nome Completo')
         return
       }
 
@@ -173,21 +182,38 @@ const Landing: React.FC = () => {
       || strCPF == "66666666666"
       || strCPF == "77777777777"
       || strCPF == "88888888888"
-      || strCPF == "99999999999") return false
+      || strCPF == "99999999999"){
+        return false
+      } 
     
       for (i=1; i<=9; i++) sum = sum + parseInt(strCPF.substring(i-1, i)) * (11 - i)
       rest = (sum * 10) % 11
     
         if ((rest == 10) || (rest == 11))  rest = 0
-        if (rest != parseInt(strCPF.substring(9, 10)) ) return false
+        if (rest != parseInt(strCPF.substring(9, 10)) ){
+          return false
+        } 
     
       sum = 0
         for (i = 1; i <= 10; i++) sum = sum + parseInt(strCPF.substring(i-1, i)) * (12 - i)
         rest = (sum * 10) % 11
     
         if ((rest == 10) || (rest == 11))  rest = 0
-        if (rest != parseInt(strCPF.substring(10, 11) ) ) return false
+        if (rest != parseInt(strCPF.substring(10, 11) ) ){
+          return false
+        } 
         return true
+  }
+
+  const checkNameAndLastName = (name: string): boolean => {
+    const regName = /^[a-zA-Z]+ [a-zA-Z]+$/
+    if(!regName.test(name)){
+      
+        return false
+    }else{
+        
+        return true
+    }
   }
 
   return (
